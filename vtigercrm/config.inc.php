b10a0b76b49e973df88892e5e5aa5b5af85675ba
unknown <thuan3615091997@gmail.com>
2021-04-04 22:09:46 +0700

    tes

--- vtigercrm/config.inc.php
+++ vtigercrm/config.inc.php
@@ -79,7 +79,7 @@ $dbconfigoption['ssl'] = false;
 
 $host_name = $dbconfig['db_hostname'];
 
-$site_URL = 'https://trinhthuan.live/vtigercrm/';
+$site_URL = 'https://trinhthuan.live/';
 
 // url for customer portal (Example: http://vtiger.com/portal)
 $PORTAL_URL = $site_URL.'/customerportal';
