0bff17edda79af9d4e372de6834705c28a72b5bd
unknown <thuan3615091997@gmail.com>
2021-04-04 21:33:58 +0700

    t

--- vtigercrm/config.inc.php
+++ vtigercrm/config.inc.php
@@ -46,9 +46,9 @@ $HELPDESK_SUPPORT_EMAIL_REPLY_ID = $HELPDESK_SUPPORT_EMAIL_ID;
 
 $dbconfig['db_server'] = 'localhost';
 $dbconfig['db_port'] = ':3306';
-$dbconfig['db_username'] = 'root';
-$dbconfig['db_password'] = '';
-$dbconfig['db_name'] = 'vcrm';
+$dbconfig['db_username'] = 'thuancrm_admin';
+$dbconfig['db_password'] = 'Thuan15091997';
+$dbconfig['db_name'] = 'thuancrm_tvts';
 $dbconfig['db_type'] = 'mysqli';
 $dbconfig['db_status'] = 'true';
 
@@ -79,12 +79,12 @@ $dbconfigoption['ssl'] = false;
 
 $host_name = $dbconfig['db_hostname'];
 
-$site_URL = 'http://localhost:8080/vtigercrm/';
+$site_URL = 'https://trinhthuan.live/vtigercrm/';
 
 // url for customer portal (Example: http://vtiger.com/portal)
 $PORTAL_URL = $site_URL.'/customerportal';
 // root directory path
-$root_directory = 'C:\xampp\htdocs\vtigercrm/';
+$root_directory = '/home/thuancrm/public_html/vtigercrm/';
 
 // cache direcory path
 $cache_dir = 'cache/';
